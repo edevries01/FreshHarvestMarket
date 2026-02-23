9c046502bb72d4c3659178e4b397dccb99b9ec0a
tfbrown-dm <tfbrown@dmacc.edu>
2026-02-22 18:29:05 -0600

    Add OrderItem model

---
+++ FreshHarvestMarket/FreshHarvestMarket/Models/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FreshHarvestMarket.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        [Range(1, 100, ErrorMessage = "Please enter a quanity between 1 and 100.")]
+        public int Quanity {  get; set; }
+
+        [Column (TypeName = "decimal(18,2)")]
+        public decimal UnitPrice { get; set; }
+
+        public decimal TotalPrice => Quanity * UnitPrice;
+
+        //Foregin Key to Order
+        public int OrderId { get; set; }
+
+        //Nullable for cart use
+        public Order? Order { get; set; }
+    }
+}
