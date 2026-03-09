db2ac6abef328439db55086d917145edf8093b90
edevries01 <edevries1@dmacc.edu>
2026-03-08 23:40:45 -0500

    Changes -Added a pickuptime to orders -Removed user stuff, there is not use in including this now because we will have to overwrite it all when we add Identity -Only got a little work done, had to think a lot. Will branch a new one and continue stuff on this Monday 3/9/2026

---
+++ FreshHarvestMarket/FreshHarvestMarket/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FreshHarvestMarket.Controllers
+{
+    /// <summary>
+    /// Controller for Order data entity
+    /// </summary>
+    public class OrderController : Controller
+    {
+        /// <summary>
+        /// For now: Returns view with all of the orders
+        /// In Future: After Identity, only return orders for signed-in user
+        /// </summary>
+        /// <returns>View with display of existing orders</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
