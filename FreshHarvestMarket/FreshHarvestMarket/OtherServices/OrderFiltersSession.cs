24dc857adcbaf80976e2f27a5d64c46c5dc53db2
edevries01 <edevries1@dmacc.edu>
2026-03-29 22:48:05 -0500

    Merging for now. I'll make a new branch to continue work on the order browser tomorrow.

---
+++ FreshHarvestMarket/FreshHarvestMarket/OtherServices/OrderFiltersSession.cs
@@ -0,0 +1,16 @@
+﻿namespace FreshHarvestMarket.OtherServices
+{
+    public class OrderFiltersSession
+    {
+        private const string OrderFiltersKey = "OrderFilters";
+
+        private ISession session { get; set; }
+
+        public OrderFiltersSession(ISession session)
+        {
+            this.session = session;
+        }
+
+
+    }
+}
