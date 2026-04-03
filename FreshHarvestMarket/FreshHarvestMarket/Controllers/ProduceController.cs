83d1f6c2fe8ca053a6c18be4bfdc2af9df95747a
tfbrown-dm <tfbrown@dmacc.edu>
2026-04-03 14:17:52 -0500

    Added produce controller

---
+++ FreshHarvestMarket/FreshHarvestMarket/Controllers/ProduceController.cs
@@ -0,0 +1,48 @@
+﻿using FreshHarvestMarket.Data;
+using FreshHarvestMarket.Models;
+using FreshHarvestMarket.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreshHarvestMarket.Controllers
+{
+    public class ProduceController : Controller
+    {
+        private FreshMarketContext _context;
+
+        public ProduceController(FreshMarketContext context)
+        {
+            _context = context;
+        }
+
+        //Retrieves a list of produce from the database, filtered by catgetory (optional)
+        public IActionResult Index(string category = "All")
+        {
+            var produceQuery = _context.Produce.AsQueryable();
+
+            if (category != "All")
+            {
+                produceQuery = produceQuery.Where(p => p.ProduceCategory == category);
+            }
+
+            var viewModel = new BrowseProduceViewModel
+            {
+                ProduceItems = produceQuery.ToList(),
+                SelectedCategory = category,
+                Categories = _context.Produce
+                    .Select(p => p.ProduceCategory)
+                    .Distinct()
+                    .ToList()
+            };
+            return View(viewModel);
+
+        }
+
+        //Finds produce item by ID. Returns NotFound if item not found.
+        public IActionResult Details(int id)
+        {
+            var item = _context.Produce.Find(id);
+            if (item == null) return NotFound();
+            return View(item);
+        }
+    }
+}
