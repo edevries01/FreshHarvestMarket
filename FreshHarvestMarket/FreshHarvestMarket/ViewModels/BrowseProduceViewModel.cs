5194442365f9ed82dd6364a4692ba3cf1fe00918
tfbrown-dm <tfbrown@dmacc.edu>
2026-04-03 14:32:00 -0500

    Added Browse produce Model

---
+++ FreshHarvestMarket/FreshHarvestMarket/ViewModels/BrowseProduceViewModel.cs
@@ -0,0 +1,16 @@
+﻿using FreshHarvestMarket.Models;
+
+namespace FreshHarvestMarket.ViewModels
+{
+    public class BrowseProduceViewModel
+    {
+        //List of items to show
+        public List<Produce> ProduceItems { get; set; } = new();
+
+        //Category the user is currently looking at. Default = All
+        public string SelectedCategory { get; set; } = "All";
+
+        //List of all categories found in the database
+        public List<string> Categories { get; set; } = new();
+    }
+}
