be1c272e8e2574c9227eb6d5e89cd9fc964468a0
Holly Dahlstrom <hmb0831@yahoo.com>
2026-04-16 19:05:30 -0500

    Edited Confirmation Page

---
+++ FreshHarvestMarket/FreshHarvestMarket/ViewModels/ConfirmationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using FreshHarvestMarket.Models;
+
+namespace FreshHarvestMarket.ViewModels
+{
+    public class ConfirmationViewModel
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public DateTime PickupDate { get; set; }
+
+        public List<CartItem> Items { get; set; } = new();
+
+        public decimal Total => Items.Sum(x => x.LineTotal);
+    }
+}
