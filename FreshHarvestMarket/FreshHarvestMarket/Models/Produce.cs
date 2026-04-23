0fcbea250fa52be6c0bc82eb627ef0c131ec1178
Holly Dahlstrom <hmb0831@yahoo.com>
2026-04-23 16:57:29 -0500

    Added Validation to Add New Produce & Edit Produce

--- FreshHarvestMarket/FreshHarvestMarket/Models/Produce.cs
+++ FreshHarvestMarket/FreshHarvestMarket/Models/Produce.cs
@@ -1,20 +1,29 @@
-﻿namespace FreshHarvestMarket.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreshHarvestMarket.Models
 {
     public class Produce
     {
         public int ProduceId { get; set; }
 
+        [Required(ErrorMessage = "Produce name is required")]
         public string ProduceName { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Description is required")]
         public string? ProduceDescription { get; set; }
 
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, 10000)]
         public decimal UnitPrice { get; set; }
 
+        [Required(ErrorMessage = "Category is required")]
         public string ProduceCategory { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "Inventory is required")]
+        [Range(0, int.MaxValue)]
         public int InventoryTotal { get; set; }
 
-        public string? ImageUrl { get; set; } = "placeholder.jpg";
+        public string? ImageUrl { get; set; }
 
         /// <summary>
         /// Navigation property for discount tied to produce item
