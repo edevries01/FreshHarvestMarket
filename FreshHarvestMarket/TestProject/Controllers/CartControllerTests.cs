df4e9cb89cbbfe3d0936e894d210542ba6d71487
edevries01 <edevries1@dmacc.edu>
2026-04-26 22:27:19 -0500

    Added more tests

---
+++ FreshHarvestMarket/TestProject/Controllers/CartControllerTests.cs
@@ -0,0 +1,144 @@
+using FreshHarvestMarket.Controllers;
+using FreshHarvestMarket.Models;
+using FreshHarvestMarket.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace TestProject.Controllers;
+
+[TestClass]
+public class CartControllerTests
+{
+    /// <summary>
+    /// GET call on Index we expect to be successful
+    /// </summary>
+    [TestMethod]
+    public void Index_Successful()
+    {
+        //ARRANGE
+        Mock<ICartService> mockCartService = new Mock<ICartService>();
+        mockCartService.Setup(m => m.GetCart()).Returns(new List<CartItem>());
+        mockCartService.Setup(m => m.GetTotal()).Returns(0);
+
+        CartController cartController = new CartController(mockCartService.Object);
+
+        //ACT
+        var result = cartController.Index();
+
+        //ASSERT
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(ViewResult));
+
+        ViewResult viewResult = (ViewResult)result;
+
+        //Assert calls
+        mockCartService.Verify(s => s.GetCart(), Times.Once());
+        mockCartService.Verify(s => s.GetTotal(), Times.Once());    
+    }
+
+    [TestMethod]
+    public void Add_Successfully()
+    {
+        //ARRANGE
+        Mock<ICartService> mockCartService = new Mock<ICartService>();
+
+        CartController cartController = new CartController(mockCartService.Object);
+
+        //ACT
+        var result = cartController.Add(1, "Carrot", "image.jpg", 1.00m, 1);
+
+        //ASSERT
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+
+        //Assert calls
+        mockCartService.Verify(s => s.AddItem(It.IsAny<CartItem>()), Times.Once());
+    }
+
+    /// <summary>
+    /// Verifies we call remove successfully
+    /// </summary>
+    [TestMethod]
+    public void Remove_Successfully()
+    {
+        //ARRANGE
+        Mock<ICartService> mockCartService = new Mock<ICartService>();
+
+        CartController cartController = new CartController(mockCartService.Object);
+
+        //ACT
+        var result = cartController.Remove(1);
+
+        //ASSERT
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+
+        mockCartService.Verify(m => m.RemoveItem(1), Times.Once);
+    }
+
+    /// <summary>
+    /// Verify we call the GET path on Checkout() correctly
+    /// </summary>
+    [TestMethod]
+    public void Checkout_Successful()
+    {
+        //ARRANGE
+        Mock<ICartService> mockCartService = new Mock<ICartService>();
+
+        CartController cartController = new CartController(mockCartService.Object);
+
+        //ACT
+        var result = cartController.Checkout();
+
+        //ASSERT
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(ViewResult));
+
+        ViewResult viewResult = (ViewResult)result;
+    }
+
+    [TestMethod]
+    public void Update_Successfully()
+    {
+        //ARRANGE
+        Mock<ICartService> mockCartService = new Mock<ICartService>();
+        mockCartService.Setup(m => m.GetCart()).Returns(new List<CartItem>() { new CartItem() { ProduceId = 1, Quantity = 10, MaxQuantity = 20} });
+        mockCartService.Setup(m => m.GetTotal()).Returns(0);
+
+        CartController cartController = new CartController(mockCartService.Object);
+
+        //ACT
+        var result = cartController.Update(1, 12);
+
+        //ASSERT
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+
+        //Assert calls
+        mockCartService.Verify(m => m.GetCart(), Times.Once());
+        mockCartService.Verify(m => m.UpdateQuantity(1, 12), Times.Once());
+    }
+
+    //Should fail to update the item since it is not in the cart, but otherwise be the same as the successful one
+    [TestMethod]
+    public void Update_UnsuccessfulBecauseItemNotFound()
+    {
+        //ARRANGE
+        Mock<ICartService> mockCartService = new Mock<ICartService>();
+        mockCartService.Setup(m => m.GetCart()).Returns(new List<CartItem>() { new CartItem() { ProduceId = 1, Quantity = 10, MaxQuantity = 20 } });
+        mockCartService.Setup(m => m.GetTotal()).Returns(0);
+
+        CartController cartController = new CartController(mockCartService.Object);
+
+        //ACT
+        var result = cartController.Update(125, 1);
+
+        //ASSERT
+        Assert.IsNotNull(result);
+        Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+
+        //Assert calls
+        mockCartService.Verify(m => m.GetCart(), Times.Once());
+        mockCartService.Verify(m => m.UpdateQuantity(125, 1), Times.Never());
+    }
+}
--- FreshHarvestMarket/TestProject/Services/CartServiceTests.cs
+++ FreshHarvestMarket/TestProject/Services/CartServiceTests.cs
@@ -3,6 +3,8 @@ using FreshHarvestMarket.Repositories;
 using FreshHarvestMarket.Services;
 using Microsoft.AspNetCore.Http;
 using Moq;
+using System.Text;
+using System.Text.Json;
 using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
 
 namespace TestProject;
@@ -17,14 +19,14 @@ public class CartServiceTests
     private IQueryable<Produce> testProduce = new List<Produce>()
     {
         new Produce()
-            {
-                ProduceId = 1,
-                ProduceName = "Zuccihini",
-                ProduceDescription = "Fresh green zucchinis",
-                UnitPrice = 1.20m,
-                ProduceCategory = "Vegetable",
-                InventoryTotal = 20
-            },
+        {
+            ProduceId = 1,
+            ProduceName = "Zuccihini",
+            ProduceDescription = "Fresh green zucchinis",
+            UnitPrice = 1.20m,
+            ProduceCategory = "Vegetable",
+            InventoryTotal = 20
+        },
         new Produce()
         {
             ProduceId = 2,
@@ -52,7 +54,7 @@ public class CartServiceTests
         {
             ProduceId = 1,
             Quantity = 10
-        }
+        },
 
     };
 
@@ -66,6 +68,7 @@ public class CartServiceTests
             cartItem.Price = testProduce.FirstOrDefault(p => p.ProduceId == cartItem.ProduceId)!.UnitPrice;
         }
     }
+
 
     /// <summary>
     /// Expects the GetCart method to execute successfully
@@ -117,18 +120,38 @@ public class CartServiceTests
         mockSession.Verify(m => m.Set(It.IsAny<string>(), It.IsAny<byte[]>()), Times.Once);
     }
 
+    /// <summary>
+    /// Checks if we successfully add an item to a cart
+    /// </summary>
     [TestMethod]
     public void AddItem_Successfil()
     {
         //ARRANGE
         LoadTestData();
         Mock<IHttpContextAccessor> mockContextAccessor = new Mock<IHttpContextAccessor>();
-        var mockSession = new Mock<ISession>();
-        var mockHttpContext = new Mock<HttpContext>();
+        DefaultHttpContext context = new DefaultHttpContext();
+        mockContextAccessor.Setup(m => m.HttpContext)
+            .Returns(context);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request)
+            .Returns(context.Request);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response)
+            .Returns(context.Response);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request.Cookies)
+            .Returns(context.Request.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response.Cookies)
+            .Returns(context.Response.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.User.Identity!.IsAuthenticated)
+            .Returns(false);
 
-        mockHttpContext.Setup(c => c.Session).Returns(mockSession.Object);
-        mockHttpContext.Setup(c => c.User.Identity.IsAuthenticated).Returns(false);
-        mockContextAccessor.Setup(a => a.HttpContext).Returns(mockHttpContext.Object);
+
+        Mock<ISession> mockSession = new Mock<ISession>();
+        mockContextAccessor.Setup(m => m.HttpContext!.Session)
+                .Returns(mockSession.Object);
+        string fakeJson = JsonSerializer.Serialize(testCartItems);
+        byte[] fakeBytes = Encoding.UTF8.GetBytes(fakeJson);
+        //Cannot mock extension methods... but with Moq we can mock the method under the extension method
+        mockSession.Setup(s => s.TryGetValue("CART", out fakeBytes)).Returns(true);
+
 
         Mock<IRepository<Produce>> mockProduceRepo = new Mock<IRepository<Produce>>();
         mockProduceRepo.Setup(m => m.GetAll()).Returns(testProduce);
@@ -143,6 +166,306 @@ public class CartServiceTests
         mockProduceRepo.Verify(m => m.GetAll(), Times.Once);
 
         List<CartItem> cart = cartService.GetCart();
-        Assert.IsTrue(cart[0] == testCartItems[0]);
+        Assert.AreEqual(cart[0].ProduceId, testCartItems[0].ProduceId);
+        Assert.AreEqual(cart[0].Quantity, testCartItems[0].Quantity);
+        mockDiscountRepo.Verify(m => m.GetAll(), Times.Never);
+    }
+
+    /// <summary>
+    /// Checks to see if we apply the discount when the user is authenticated
+    /// </summary>
+    [TestMethod]
+    public void AddItem_ApplyDiscountWhenAuthenticated()
+    {
+        //ARRANGE
+        LoadTestData();
+        Mock<IHttpContextAccessor> mockContextAccessor = new Mock<IHttpContextAccessor>();
+        DefaultHttpContext context = new DefaultHttpContext();
+        mockContextAccessor.Setup(m => m.HttpContext)
+            .Returns(context);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request)
+            .Returns(context.Request);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response)
+            .Returns(context.Response);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request.Cookies)
+            .Returns(context.Request.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response.Cookies)
+            .Returns(context.Response.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.User.Identity!.IsAuthenticated)
+            .Returns(true);
+
+
+        Mock<ISession> mockSession = new Mock<ISession>();
+        mockContextAccessor.Setup(m => m.HttpContext!.Session)
+                .Returns(mockSession.Object);
+        string fakeJson = JsonSerializer.Serialize(testCartItems);
+        byte[] fakeBytes = Encoding.UTF8.GetBytes(fakeJson);
+        //Cannot mock extension methods... but with Moq we can mock the method under the extension method
+        mockSession.Setup(s => s.TryGetValue("CART", out fakeBytes)).Returns(true);
+
+
+        Mock<IRepository<Produce>> mockProduceRepo = new Mock<IRepository<Produce>>();
+        mockProduceRepo.Setup(m => m.GetAll()).Returns(testProduce);
+        Mock<IRepository<Discount>> mockDiscountRepo = new Mock<IRepository<Discount>>();
+
+        ICartService cartService = new CartService(mockContextAccessor.Object, mockProduceRepo.Object, mockDiscountRepo.Object);
+
+        //ACT
+        cartService.AddItem(testCartItems[0]);
+
+        //ASSERT
+        //Should grab discounts if we were applying them
+        mockDiscountRepo.Verify(m => m.GetAll(), Times.Once);
+    }
+
+    [TestMethod]
+    public void RemoveItem_Successfully()
+    {
+        //ARRANGE
+        LoadTestData();
+        Mock<IHttpContextAccessor> mockContextAccessor = new Mock<IHttpContextAccessor>();
+        DefaultHttpContext context = new DefaultHttpContext();
+        mockContextAccessor.Setup(m => m.HttpContext)
+            .Returns(context);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request)
+            .Returns(context.Request);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response)
+            .Returns(context.Response);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request.Cookies)
+            .Returns(context.Request.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response.Cookies)
+            .Returns(context.Response.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.User.Identity!.IsAuthenticated)
+            .Returns(true);
+
+        Mock<ISession> mockSession = new Mock<ISession>();
+
+        //I was having trouble figuring out how to handle testing around ISession
+        //The Murach textbook talks about this time, but in a limited capacity
+        //I asked Claude how people can handle mimicing Get/Set with byte data, and it showed me code simialar to this
+        Dictionary<string, byte[]> sessionStorage = new Dictionary<string, byte[]>();
+        mockSession.Setup(s => s.Set(It.IsAny<string>(), It.IsAny<byte[]>()))
+        .Callback<string, byte[]>((key, value) => sessionStorage[key] = value);
+
+        mockSession.Setup(s => s.TryGetValue(It.IsAny<string>(), out It.Ref<byte[]>.IsAny))
+            .Returns((string key, out byte[] value) =>
+            {
+                var found = sessionStorage.TryGetValue(key, out value!);
+                return found;
+            });
+        mockContextAccessor.Setup(m => m.HttpContext!.Session)
+                .Returns(mockSession.Object);
+
+        Mock<IRepository<Produce>> mockProduceRepo = new Mock<IRepository<Produce>>();
+        mockProduceRepo.Setup(m => m.GetAll()).Returns(testProduce);
+        Mock<IRepository<Discount>> mockDiscountRepo = new Mock<IRepository<Discount>>();
+
+        ICartService cartService = new CartService(mockContextAccessor.Object, mockProduceRepo.Object, mockDiscountRepo.Object);
+        cartService.AddItem(testCartItems[0]);
+        //ACT
+        cartService.RemoveItem(1);
+
+        //ASSERT
+        List<CartItem> cart = cartService.GetCart();
+        Assert.IsTrue(cart.Count == 0);
+    }
+
+    [TestMethod]
+    public void GetTotal_Successfully()
+    {
+        //ARRANGE
+        LoadTestData();
+        Mock<IHttpContextAccessor> mockContextAccessor = new Mock<IHttpContextAccessor>();
+        DefaultHttpContext context = new DefaultHttpContext();
+        mockContextAccessor.Setup(m => m.HttpContext)
+            .Returns(context);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request)
+            .Returns(context.Request);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response)
+            .Returns(context.Response);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request.Cookies)
+            .Returns(context.Request.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response.Cookies)
+            .Returns(context.Response.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.User.Identity!.IsAuthenticated)
+            .Returns(true);
+
+        Mock<ISession> mockSession = new Mock<ISession>();
+
+        //I was having trouble figuring out how to handle testing around ISession
+        //The Murach textbook talks about this time, but in a limited capacity
+        //I asked Claude how people can handle mimicing Get/Set with byte data, and it showed me code simialar to this
+        Dictionary<string, byte[]> sessionStorage = new Dictionary<string, byte[]>();
+        mockSession.Setup(s => s.Set(It.IsAny<string>(), It.IsAny<byte[]>()))
+        .Callback<string, byte[]>((key, value) => sessionStorage[key] = value);
+
+        mockSession.Setup(s => s.TryGetValue(It.IsAny<string>(), out It.Ref<byte[]>.IsAny))
+            .Returns((string key, out byte[] value) =>
+            {
+                var found = sessionStorage.TryGetValue(key, out value!);
+                return found;
+            });
+        mockContextAccessor.Setup(m => m.HttpContext!.Session)
+                .Returns(mockSession.Object);
+
+        Mock<IRepository<Produce>> mockProduceRepo = new Mock<IRepository<Produce>>();
+        mockProduceRepo.Setup(m => m.GetAll()).Returns(testProduce);
+        Mock<IRepository<Discount>> mockDiscountRepo = new Mock<IRepository<Discount>>();
+
+        ICartService cartService = new CartService(mockContextAccessor.Object, mockProduceRepo.Object, mockDiscountRepo.Object);
+        cartService.AddItem(testCartItems[0]);
+        //ACT
+        decimal total = cartService.GetTotal();
+
+        //ASSERT
+        Assert.IsTrue(total == 12);
+    }
+
+    [TestMethod]
+    public void ClearCart_Successfully()
+    {
+        //ARRANGE
+        LoadTestData();
+        Mock<IHttpContextAccessor> mockContextAccessor = new Mock<IHttpContextAccessor>();
+        DefaultHttpContext context = new DefaultHttpContext();
+        mockContextAccessor.Setup(m => m.HttpContext)
+            .Returns(context);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request)
+            .Returns(context.Request);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response)
+            .Returns(context.Response);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request.Cookies)
+            .Returns(context.Request.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response.Cookies)
+            .Returns(context.Response.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.User.Identity!.IsAuthenticated)
+            .Returns(true);
+
+        Mock<ISession> mockSession = new Mock<ISession>();
+        mockContextAccessor.Setup(m => m.HttpContext!.Session)
+                .Returns(mockSession.Object);
+
+
+        Mock<IRepository<Produce>> mockProduceRepo = new Mock<IRepository<Produce>>();
+        Mock<IRepository<Discount>> mockDiscountRepo = new Mock<IRepository<Discount>>();
+
+        ICartService cartService = new CartService(mockContextAccessor.Object, mockProduceRepo.Object, mockDiscountRepo.Object);
+        
+        //ACT
+        cartService.ClearCart();
+
+        //ASSERT
+        mockSession.Verify(m => m.Remove("CART"), Times.Once());
+    }
+
+    [TestMethod]
+    public void UpdateQuantity_Successfully()
+    {
+        //ARRANGE
+        LoadTestData();
+        Mock<IHttpContextAccessor> mockContextAccessor = new Mock<IHttpContextAccessor>();
+        DefaultHttpContext context = new DefaultHttpContext();
+        mockContextAccessor.Setup(m => m.HttpContext)
+            .Returns(context);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request)
+            .Returns(context.Request);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response)
+            .Returns(context.Response);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request.Cookies)
+            .Returns(context.Request.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response.Cookies)
+            .Returns(context.Response.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.User.Identity!.IsAuthenticated)
+            .Returns(true);
+
+        Mock<ISession> mockSession = new Mock<ISession>();
+
+        //I was having trouble figuring out how to handle testing around ISession
+        //The Murach textbook talks about this time, but in a limited capacity
+        //I asked Claude how people can handle mimicing Get/Set with byte data, and it showed me code simialar to this
+        Dictionary<string, byte[]> sessionStorage = new Dictionary<string, byte[]>();
+        mockSession.Setup(s => s.Set(It.IsAny<string>(), It.IsAny<byte[]>()))
+        .Callback<string, byte[]>((key, value) => sessionStorage[key] = value);
+
+        mockSession.Setup(s => s.TryGetValue(It.IsAny<string>(), out It.Ref<byte[]>.IsAny))
+            .Returns((string key, out byte[] value) =>
+            {
+                var found = sessionStorage.TryGetValue(key, out value!);
+                return found;
+            });
+        mockContextAccessor.Setup(m => m.HttpContext!.Session)
+                .Returns(mockSession.Object);
+
+        Mock<IRepository<Produce>> mockProduceRepo = new Mock<IRepository<Produce>>();
+        mockProduceRepo.Setup(m => m.GetAll()).Returns(testProduce);
+        Mock<IRepository<Discount>> mockDiscountRepo = new Mock<IRepository<Discount>>();
+
+        ICartService cartService = new CartService(mockContextAccessor.Object, mockProduceRepo.Object, mockDiscountRepo.Object);
+        //Add single item to cart
+        cartService.AddItem(testCartItems[0]);
+
+        //ACT
+        cartService.UpdateQuantity(1, 100); //More than what is in inventory
+
+        //ASSERT
+        List<CartItem> cart = cartService.GetCart();
+        //Since it is more than what is in inventory, it should default to the max left
+        Assert.IsTrue(cart[0].Quantity == 20);
+    }
+
+    [TestMethod]
+    public void UpdateQuantity_FailDueToUnknownProduce()
+    {
+        //ARRANGE
+        LoadTestData();
+        Mock<IHttpContextAccessor> mockContextAccessor = new Mock<IHttpContextAccessor>();
+        DefaultHttpContext context = new DefaultHttpContext();
+        mockContextAccessor.Setup(m => m.HttpContext)
+            .Returns(context);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request)
+            .Returns(context.Request);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response)
+            .Returns(context.Response);
+        mockContextAccessor.Setup(m => m.HttpContext!.Request.Cookies)
+            .Returns(context.Request.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.Response.Cookies)
+            .Returns(context.Response.Cookies);
+        mockContextAccessor.Setup(m => m.HttpContext!.User.Identity!.IsAuthenticated)
+            .Returns(true);
+
+        Mock<ISession> mockSession = new Mock<ISession>();
+
+        //I was having trouble figuring out how to handle testing around ISession
+        //The Murach textbook talks about this time, but in a limited capacity
+        //I asked Claude how people can handle mimicing Get/Set with byte data, and it showed me code simialar to this
+        Dictionary<string, byte[]> sessionStorage = new Dictionary<string, byte[]>();
+        mockSession.Setup(s => s.Set(It.IsAny<string>(), It.IsAny<byte[]>()))
+        .Callback<string, byte[]>((key, value) => sessionStorage[key] = value);
+
+        mockSession.Setup(s => s.TryGetValue(It.IsAny<string>(), out It.Ref<byte[]>.IsAny))
+            .Returns((string key, out byte[] value) =>
+            {
+                var found = sessionStorage.TryGetValue(key, out value!);
+                return found;
+            });
+        mockContextAccessor.Setup(m => m.HttpContext!.Session)
+                .Returns(mockSession.Object);
+
+        Mock<IRepository<Produce>> mockProduceRepo = new Mock<IRepository<Produce>>();
+        mockProduceRepo.Setup(m => m.GetAll()).Returns(testProduce);
+        Mock<IRepository<Discount>> mockDiscountRepo = new Mock<IRepository<Discount>>();
+
+        ICartService cartService = new CartService(mockContextAccessor.Object, mockProduceRepo.Object, mockDiscountRepo.Object);
+        //Add single item to cart
+        cartService.AddItem(testCartItems[0]);
+
+        //ACT
+        cartService.UpdateQuantity(153, 100); //Invalid produce
+
+        //ASSERT
+        List<CartItem> cart = cartService.GetCart();
+        //Should be same as before test
+        Assert.IsTrue(cart.Count == 1);
+        Assert.IsFalse(cart.Any(ci => ci.ProduceId == 153));
     }
 }
