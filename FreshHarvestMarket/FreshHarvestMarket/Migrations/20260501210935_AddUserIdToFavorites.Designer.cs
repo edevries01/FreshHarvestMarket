6a2d5cd6c26c3457111e2545d23bf18b06fed3d4
Holly Dahlstrom <hmb0831@yahoo.com>
2026-05-01 21:14:47 -0500

    Fixed user ties to Favorites and access to it

--- FreshHarvestMarket/FreshHarvestMarket/Migrations/20260501210935_AddUserIdToFavorites.Designer.cs
+++ FreshHarvestMarket/FreshHarvestMarket/Migrations/20260501210935_AddUserIdToFavorites.Designer.cs
@@ -4,6 +4,7 @@ using FreshHarvestMarket.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FreshHarvestMarket.Migrations
 {
     [DbContext(typeof(FreshHarvestContext))]
-    partial class FreshMarketContextModelSnapshot : ModelSnapshot
+    [Migration("20260501210935_AddUserIdToFavorites")]
+    partial class AddUserIdToFavorites
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
